6dfb1412ec0ad887ec79fa5aecb14e86079ae2c6
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-10-06 22:59:44 +0200

    base files

---
+++ comandline/Program.cs
@@ -0,0 +1,35 @@
+﻿using Interprocomm;
+using System;
+using System.Threading;
+
+namespace comandline
+{
+    internal class Program
+    {
+        #region Private Fields
+
+        private static Mutex mutex;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+            bool created;
+            mutex = new Mutex(true, "{7b1a161b-30cc-4679-856b-30953873033d}", out created);
+            if (created)
+            {
+                var server = new Server("{7b1a161b-30cc-4679-856b-30953873033d}");
+                //server code...
+            }
+            else
+            {
+                var client = new Client("{7b1a161b-30cc-4679-856b-30953873033d}");
+                //client code...
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
