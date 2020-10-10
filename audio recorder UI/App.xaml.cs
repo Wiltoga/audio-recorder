753a434bc4c4ba0b4bfb99acd175b3ccbb47de55
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-10-10 16:38:09 +0200

    remade all the base files

---
+++ audio recorder UI/App.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Interprocomm;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace audio_recorder_UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Private Fields
+
+        private Mutex mutex;
+        private Mutex uiMutex;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public App()
+        {
+            bool created;
+            uiMutex = new Mutex(true, "audioRecorderUI", out created);
+            if (created)
+            {
+                UIServer = new Server("audioRecorderUI", 1);
+                UIServer.RequestRecieved += r =>
+                {
+                    var code = BitConverter.ToInt32(r.Data, 0);
+                    switch (code)
+                    {
+                        case 1:
+                            //wake up app
+                            break;
+                    }
+                };
+                mutex = new Mutex(true, "audioRecorder", out created);
+                if (created)
+                {
+                    mutex.ReleaseMutex();
+                    mutex.Close();
+                    Process.Start(new ProcessStartInfo("audiorec.exe"));
+                }
+                Client = new Client("audioRecorder");
+                Client.Start();
+                _ = UIServer.Start();
+            }
+            else
+            {
+                Client uiClient = new Client("audioRecorderUI");
+                uiClient.Start();
+                uiClient.SendRequest(BitConverter.GetBytes(1));
+                Environment.Exit(0);
+            }
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public Client Client { get; private set; }
+        public Server UIServer { get; private set; }
+
+        #endregion Public Properties
+    }
+}
---
+++ audio recorder cmd/Program.cs
@@ -0,0 +1,43 @@
+﻿using Interprocomm;
+using System;
+using System.Threading;
+
+namespace audioRecorderCmd
+{
+    internal class Program
+    {
+        #region Private Fields
+
+        private static Mutex mutex;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        public static Client Client { get; private set; }
+        public static Server Server { get; private set; }
+
+        #endregion Public Properties
+
+        #region Private Methods
+
+        private static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            bool created;
+            mutex = new Mutex(true, "audioRecorder", out created);
+            if (created)
+            {
+                Server = new Server("audioRecorder", 2);
+
+                await Server.Start();
+            }
+            else
+            {
+                Client = new Client("audioRecorder");
+                Client.Start();
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
